25faa5c5c842cad110e8af10660a8e9377a80cf7
Sebin <sebinshaiju77@gmail.com>
2024-02-28 22:23:34 +0530

    1

--- lib/widgets/trending.dart
+++ lib/widgets/trending.dart
@@ -14,7 +14,36 @@ class TrendingMovies extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          modtext(text:"Trending Movies",color: Colors.white,size:30,)
+          modtext(text:"Trending Movies",color: Colors.white,size:30,),
+          Container(height: 270,
+          child:ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: trending.length,
+            itemBuilder: (context,index){
+              return InkWell(
+                onTap: (){},
+                child: Container(
+                  width: 140,
+                  child: Column(
+                    children: [
+                      Container(
+                        height:200,
+                        decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(
+                          'http://image.tmdb.org/t/p/w500'+trending[index]['poster_path']
+                        ),),),
+                      ),
+                      Container(
+                        child: modtext(text:trending[index]['title']!=null?
+                            trending[index]['title']:'loading',color: Colors.white,size: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          ),
+          )
         ],
       ),
     );
